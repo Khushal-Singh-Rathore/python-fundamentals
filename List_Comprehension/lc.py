77c0d212fbb59e81bb164c39d200eebf6c76bfca
Khushal Singh Rathore <khushalrathore1515@gmail.com>
2026-02-02 21:52:10 +0530

    concatnation

--- List_Comprehension/lc.py
+++ List_Comprehension/lc.py
@@ -6,30 +6,30 @@ inventory = {
     "C05": {"brand": "Hyundai", "model": "Verna", "color": "white", "price": 18000},
 }
 
-# # // Filtering
+# // Filtering
 
-# expensive_cars = {
-#     id: details for id, details in inventory.items() if details["price"] > 30000
-# }
-# print(expensive_cars)
+expensive_cars = {
+    id: details for id, details in inventory.items() if details["price"] > 30000
+}
+print(expensive_cars)
 
 
-# # Filtering + Converting [ her we are filtering and converting the result in list]
+# Filtering + Converting [ her we are filtering and converting the result in list]
 
-# white_cars = [
-#     {"id": c_id, **details}
-#     for c_id, details in inventory.items()
-#     if details["color"] == "white"
-# ]
-# print(white_cars)
+white_cars = [
+    {"id": c_id, **details}
+    for c_id, details in inventory.items()
+    if details["color"] == "white"
+]
+print(white_cars)
 
-## Advanced Transformation (Selecting Specific Fields)
+# Advanced Transformation (Selecting Specific Fields)
 
-# car_models = [details["brand"].upper() for details in inventory.values()]
+car_models = [details["brand"].upper() for details in inventory.values()]
 
-# print(car_models)
+print(car_models)
 
-## Sorting (Using sorted() with a List)
+# Sorting (Using sorted() with a List)
 
 all_cars = [{"id": car_id, **details} for car_id, details in inventory.items()]
 
